aa3e83416b1dcc58c37efde5937e06a0aa831113
unknown <samuel.haymond@gmail.com>
2022-03-02 13:03:37 -0700

    state section

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,23 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-user></app-user>
+               <app-quote></app-quote>
+               <h2>The Quote</h2>
+               <p>A Wonderful Quote</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
+    import Quote from './components/Quote.vue';
 
     export default {
         components: {
-            appUser: User
+            appQuote: Quote
         }
     }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
 </style>
