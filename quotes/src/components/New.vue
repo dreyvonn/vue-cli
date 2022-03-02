eceb46f49ea1d1d51a456fd0c88225589f9817a5
unknown <samuel.haymond@gmail.com>
2022-03-02 15:17:18 -0700

    added folder

---
+++ quotes/src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>New Quote</h1>
+        <button @click="counter++">Increase</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>
+export default ({
+    data: function() {
+        return {
+            counter: 0
+        };
+    },
+    destroyed() {
+        console.log('Destrtoyed');
+    },
+    deactivated() {
+        console.log('Deactivated');
+    },
+    activated() {
+        console.log('Activated');
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
