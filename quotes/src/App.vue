36ce41da1be2c060dae648a7d491fb122ee120bc
unknown <samuel.haymond@gmail.com>
2022-03-02 16:54:29 -0700

    quotes app

--- quotes/src/App.vue
+++ quotes/src/App.vue
@@ -1,47 +1,46 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote($event)"></app-quote-grid>
         <div class="row">
-            <div class="col-xs-12">
-                <button @click="selectedComponent = 'app-quote'">Quote</button>
-                <button @click="selectedComponent = 'app-author'">
-                    Author
-                </button>
-                <button @click="selectedComponent = 'app-new'">New</button>
-                <hr />
-                <p>{{ selectedComponent }}</p>
-                <!-- <app-quote>
-                    <h2 slot="title">{{ quoteTitle }}</h2>
-                    <p>A Wonderful Quote</p>
-                </app-quote> -->
-                <keep-alive>
-                    <component :is="selectedComponent">
-                        <p>Default content</p>
-                    </component>
-                </keep-alive>
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Quote from "./components/Quote.vue";
-import Author from "./components/Author.vue";
-import New from "./components/New.vue";
+    import QuoteGrid from "./components/QuoteGrid.vue";
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
 
-export default {
-    data: function () {
-        return {
-            quoteTitle: "The Quote",
-            selectedComponent: "app-quote",
-        };
-    },
-    components: {
-        appQuote: Quote,
-        appAuthor: Author,
-        appNew: New,
-    },
-};
+    export default {
+        data: function () {
+            return {
+                maxQuotes: 10,
+                quotes: ["just a quote to see something"]
+            };
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(i) {
+                this.quotes.splice(i, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        },
+    };
 </script>
 
-<style scoped>
+<style>
 </style>
--- quotes/src/components/Quote.vue
+++ quotes/src/components/Quote.vue
@@ -1,36 +1,29 @@
 <template>
-    <div>
-        <div class="title">
-            <slot name="title"></slot>
-            <span style="color: #ccc"><slot name="subtitle">The Subtitle</slot></span>
-        </div>
-        <hr>
-        <div>
-            <slot></slot>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+    export default {};
 </script>
 
 <style scoped>
-div {
-    border: 1px solid #ccc;
-    box-shadow: 1px 1px 2px;
-    padding: 30px;
-    margin: 30px auto;
-    text-align: center;
-}
+    .panel-body {
+        font-family: "Arizonia", cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
 
-h2 {
-    color: red;
-}
+    .quote {
+        cursor: pointer;
+    }
 
-.title {
-    font-style: italic;
-}
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
 </style>
